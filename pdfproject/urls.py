4d06b6c691eff65883806202fa74c5198e3f98fd
rexonantony <rexonantony@example.com>
2024-06-11 10:20:38 +0530

    one

--- pdfproject/urls.py
+++ pdfproject/urls.py
@@ -5,8 +5,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('front/', views.front, name='front'),
-    path('split_files/', views.split_pdf, name='split_pdf'),
+    path('front/', views.front, name='front'),# Maps the URL 'front/' to the front view function and names it 'front'
+    path('split_files/', views.split_pdf, name='split_pdf'),# Maps the URL 'split_files/' to the split_pdf view function and names it 'split_pdf'
 ]
+
 if settings.DEBUG :
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)# Adds URL patterns to serve media files from MEDIA_ROOT during development
